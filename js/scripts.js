6195d07a10aeae801359f4fdb3b37fac52778fc9
Drake Wilcox and jeremy kale padot <jeremy@>
2020-01-16 13:32:24 -0800

    add working radio buttons and img selector

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event)  {
     event.preventDefault();
-      
 
+    var animal = $("input:radio[name=animal]:checked").val();
+    if (animal === "turtle") {
+    $(".yungTurtle").show(); 
+    $(".dragonboi").hide();
+  }
+  if (animal === "dragon"){
+    $(".dragonboi").show();
+    $(".yungTurtle").hide()
+    alert("wow!");
+  }
+  alert("wow!");
   })
 });
